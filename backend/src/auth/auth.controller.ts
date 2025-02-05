69f824b667b397454b6650f2d5ea2c5c33009f7b
Gaurav Tak <gaurav.wisflux@gmail.com>
2025-02-05 18:14:37 +0530

    impoted interfaces defined for login & signup

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,18 +1,22 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {
+  ILoginRequest,
+  ILoginResponse,
+  ISignupRequest,
+} from './auth.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signup(@Body() body: { username: string; email: string; password:string}) {
-    const { username, email, password} = body;
-    return this.authService.signup(username, email, password);
+  async signup(@Body() body: ISignupRequest) {
+    return this.authService.signup(body);
   }
 
-  //@Get('login')
-  //login(): string {
-  //  return this.authService.login();
-  //}
+  @Post('login')
+  async login(@Body() body: ILoginRequest): Promise<ILoginResponse> {
+    return this.authService.login(body);
+  }
 }
