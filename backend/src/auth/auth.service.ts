21fb7f6a973a0bbabb4749e7e5f0249f7132ac19
Gaurav Tak <gaurav.wisflux@gmail.com>
2025-02-05 18:16:58 +0530

    created login method

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,20 +3,25 @@ import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
 import * as bcrypt from 'bcrypt';
 import { BadRequestException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import {
+  ILoginRequest,
+  ILoginResponse,
+  ISignupRequest,
+} from './auth.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User) private userModel: typeof User) {}
+  constructor(
+    @InjectModel(User) private userModel: typeof User,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async signup(
-    username: string,
-    email: string,
-    password: string,
-  ): Promise<Omit<User, 'password'> | void> {
+  async signup(req: ISignupRequest): Promise<Omit<User, 'password'> | void> {
+    const { username, email, password } = req;
     try {
       if (!email) {
         throw new BadRequestException('Email is required');
-        
       }
       const existingUser = await this.userModel.findOne({ where: { email } });
       if (existingUser) {
@@ -41,7 +46,34 @@ export class AuthService {
     }
   }
 
-  //login(): string {
-  //  return 'Login User';
-  //}
+  async login(req: ILoginRequest): Promise<ILoginResponse> {
+    const { email, password } = req;
+    try {
+      if (!email) {
+        throw new BadRequestException('Email is required');
+      }
+      if (!password) {
+        throw new BadRequestException('Password is required');
+      }
+      const user = await this.userModel.findOne({ where: { email } });
+      if (!user) {
+        throw new BadRequestException('User with this email does not exist');
+      }
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        throw new BadRequestException('Password is incorrect');
+      }
+
+      const payload = { email: user.email, id: user.id };
+      const token = this.jwtService.sign(payload);
+
+      return {
+        message: 'Login successful',
+        access_token: token,
+      };
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
 }
