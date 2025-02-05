edca3faebfe876c6aafb56e39e8aa44b72eb0bc0
Gaurav Tak <gaurav.wisflux@gmail.com>
2025-02-05 18:23:27 +0530

    handling post request on login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,46 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-login",
+  imports: [FormsModule, CommonModule],
   templateUrl: "./login.component.html",
-  styleUrl: './login.component.css'
+  styleUrl: "./login.component.css",
 })
-export class LoginComponent { 
+
+export class LoginComponent {
   title = "Login Page";
+
+  email: string = "";
+  password: string = "";
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
+  onSubmit() {
+    const formData = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post("http://localhost:4000/auth/login", formData).subscribe({
+      next: (res: any) => {
+        console.log("Login Success", res);
+        if (res && res.access_token) {
+          localStorage.setItem("access_token", res.access_token);
+          this.router.navigate(["/cart"]);
+        }
+        return res;
+      },
+      error: (e) => console.error("Login Failed!", e),
+      complete: () => {
+        console.info("Login Completed");
+      },
+    });
+  }
 }
