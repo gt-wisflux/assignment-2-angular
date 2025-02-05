a3e582c6b4507cea3ad3c880d103146f771fd067
Gaurav Tak <gaurav.wisflux@gmail.com>
2025-02-05 18:15:53 +0530

    add JwtModule to imports

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './user.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    JwtModule.register({ secret: 'secret' }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
